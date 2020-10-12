15363096f7482761f7101c8a37a3ffb1fe6d8365
Leiliu99 <liulei.liu@mail.utoronto.ca>
2020-10-12 14:36:06 -0400

    lab2 completed

--- deliver.c
+++ deliver.c
@@ -13,6 +13,7 @@
 #include <sys/resource.h>
 
 #define MAXBUFLEN 1500
+#define FRAG_SIZE 1000
 struct packet{
     unsigned int total_frag; //total number of fragments
     unsigned int frag_no; //sequence number of the fragment
@@ -21,35 +22,12 @@ struct packet{
     char filedata[1000]; //actual data within the file
 };
 
-size_t structTostring(struct packet packetFile, char* messageString){
-    char totalPackageString[10];
-    char fragNoString[10];
-    char sizeString[10];
-    char separateColon = ':';
-    sprintf(totalPackageString, "%d", packetFile.total_frag);
-    strcat(messageString, totalPackageString);
-    strncat(messageString, &separateColon, 1);
-    sprintf(fragNoString, "%d", packetFile.frag_no);
-    strcat(messageString, fragNoString);
-    strncat(messageString, &separateColon, 1);
-    sprintf(sizeString, "%d", packetFile.size);
-    strcat(messageString, sizeString);
-    strncat(messageString, &separateColon, 1);
-
-    strcat(messageString, packetFile.filename);
-    strncat(messageString, &separateColon, 1);
-
-    int partialSize = strlen(messageString);
-
-    memmove(messageString+strlen(messageString), packetFile.filedata, packetFile.size);
-    printf("converting result, %s\n", messageString);
-
-    return (partialSize + packetFile.size);
-}
-
-void send_packet(){
-
-}
+//read the required file, put content in an array for further analysis
+void read_file(unsigned char **dataArray, int *fileSize, char *fileName);
+//add required information to the packet
+void add_info_to_packet(struct packet* filePacket, int total_frag, int frag_no, int size, char* filename);
+//transfer the struct packet to string format
+structTostring(struct packet packetFile, char* messageString);
 
 int main(int argc, char** argv){
     int socketfd;
@@ -94,93 +72,39 @@ int main(int argc, char** argv){
 
     if(access(userInputFile, F_OK) != -1){
         if(strcmp(userInputFTP, msg) == 0){//input start with ftp
-            FILE* fp = fopen(userInputFile, "r");//read the input file
-
-            //calculating the size of the file
-            fseek(fp, 0L, SEEK_END);
-            long int fileSize = ftell(fp);
-            rewind(fp);
-            printf("Total filesize: %ld\n", fileSize);
+            int* fileSize = (int *)malloc(sizeof(int));
+            unsigned char *dataArray = (unsigned char *)malloc(sizeof(unsigned char));
+            read_file(&dataArray, fileSize, userInputFile);
+            printf("Total filesize: %d\n", *fileSize);
             //calculating the total package needed
-            int totalPackage;
-            if(fileSize % 1000 == 0){
-                totalPackage = fileSize / 1000;
-            }else{
-                totalPackage = fileSize / 1000 + 1;
-            }
-            int byteCounts = 0;
-            int sequenceNumber = 1;
-            char ch;
-            byteCounts = 0;
-            //creating file packet
-            struct packet filePacket;
-            while((ch = fgetc(fp)) != EOF){
-                filePacket.filedata[byteCounts] = ch;
-                printf("test %d, %c\n", byteCounts, ch);
-                byteCounts++;
-                if(byteCounts == 1000){
-                    byteCounts = 0;
-                    filePacket.size = 1000;
-                    filePacket.total_frag = totalPackage;
-                    filePacket.frag_no = sequenceNumber;
-                    filePacket.filename = userInputFile;
-                    //transfer the previous packet to string
-                    for(int i = 0; i < 10; i++){
-                        printf("%c", filePacket.filedata[i]);
-                    }
-                    printf("\n");
-                    char messageString[1500] = {};
-                    int messageSize = structTostring(filePacket, messageString);
-                    //send the message to deliver
-                    bool recievedAck = false;
-                    printf("sending message: %s\n", messageString);
-                    while(!recievedAck){
-                        //calculating the RTT time
-                        getrusage(RUSAGE_SELF, &usage);
-                        start = usage.ru_utime;
-                        timeStart = start.tv_usec;
-                        if(sendto(socketfd, messageString, messageSize, 0, servinfo->ai_addr, servinfo->ai_addrlen) == -1){
-                            printf("Unable to send packet to server. Quit the program\n");
-                            return 0;
-                        }
-                        printf("Waiting for server's acknowledge...\n");
 
-                        //receive a message from the server
-                        char buf[MAXBUFLEN];
-                        int numbytes;
-                        if((numbytes = recvfrom(socketfd, buf, MAXBUFLEN - 1, 0,(struct sockaddr *)&their_addr, &addr_len)) < 0){//receive a message from server
-                            recievedAck = false;
-                            printf("Resending message: %s\n", messageString);
-                            continue;
-                        }
-                        buf[numbytes] = '\0';
-                        printf("buf, %s\n", buf);
-                        if(strcmp(buf, "ACK") == 0){//packet sent is good
-                            printf("Recieved ack from server\n");
-                            getrusage(RUSAGE_SELF, &usage);
-                            end = usage.ru_utime;
-                            timeEnd = end.tv_usec;
-                            printf("One round is %d microseconds\n", (int)(timeEnd-timeStart));
-                            recievedAck = true;
-                            break;
-                        }
-                    }
-                    //initialize new packet
-                    struct packet filePacket;
-                    sequenceNumber++;
-                }
+            int totalPackage = *fileSize / FRAG_SIZE;
+            int remainingSize = *fileSize - totalPackage * FRAG_SIZE;
+            if(remainingSize != 0){
+                totalPackage ++;
             }
-            if(byteCounts != 0){
-                filePacket.size = byteCounts;
-                 filePacket.total_frag = totalPackage;
-                 filePacket.frag_no = sequenceNumber;
-                 filePacket.filename = userInputFile;
-                //transfer the previous packet to string
+            printf("Total packages: %d\n", totalPackage);
+            int sequenceNumber = 1;
+            for(int i = 0; i < totalPackage; i++, sequenceNumber++){
+                int packetSize;
+                if((i == totalPackage - 1)  && (remainingSize != 0)){//the only or the last package
+                    packetSize = remainingSize;
+                }else{
+                    packetSize = FRAG_SIZE;
+                }
+                int dataPos = i * FRAG_SIZE;
+
+                struct packet filePacket;//create a new package for this round
+                for(int k = 0; k < packetSize; k++){
+                    filePacket.filedata[k] = dataArray[dataPos + k];
+                }
+                add_info_to_packet(&filePacket, totalPackage, sequenceNumber, packetSize, userInputFile);
                 char messageString[1500] = {};
                 int messageSize = structTostring(filePacket, messageString);
                 //send the message to deliver
-                printf("sending message: %s\n", messageString);
                 bool recievedAck = false;
+                //printf("sending message: %s, and message size: %d\n", messageString, messageSize);
+                printf("sending package: %d, package size: %d\n", sequenceNumber, messageSize);
                 while(!recievedAck){
                     //calculating the RTT time
                     getrusage(RUSAGE_SELF, &usage);
@@ -209,10 +133,10 @@ int main(int argc, char** argv){
                         timeEnd = end.tv_usec;
                         printf("One round is %d microseconds\n", (int)(timeEnd-timeStart));
                         recievedAck = true;
+                        break;
                     }
-                }//for send data
-            }//for if bytes != 0
-            fclose(fp);
+                }
+            }
         }else{
               printf("Input must start with FTP. Quit the program\n");
               return 0;
@@ -223,5 +147,65 @@ int main(int argc, char** argv){
     }
 
     return 0;
-
 }
+
+void read_file(unsigned char **dataArray, int *fileSize, char *fileName){
+    // https://www.daniweb.com/programming/software-development/threads/188487/reading-binary-file-without-knowing-file-format
+    // start read file
+    FILE *fp;
+    fp = fopen(fileName,"rb");
+
+    //Get file length
+    fseek(fp, 0, SEEK_END);
+    *fileSize = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    // Allocate memory
+    *dataArray = (unsigned char *) malloc((*fileSize + 1) * sizeof(unsigned char));
+    if (! *dataArray) {
+        fprintf(stderr, "Memory error!");
+        fclose(fp);
+        exit(1);
+    }
+
+    // Read file contents into buffer
+    fread(*dataArray, *fileSize, 1, fp);
+    fclose(fp);
+}
+
+void add_info_to_packet(struct packet* filePacket, int total_frag, int frag_no, int size, char* filename){
+    filePacket->total_frag = total_frag;
+    filePacket->frag_no = frag_no;
+    filePacket->size = size;
+    filePacket->filename = filename;
+}
+
+size_t structTostring(struct packet packetFile, char* messageString){
+    char totalPackageString[10];
+    char fragNoString[10];
+    char sizeString[10];
+    char separateColon = ':';
+    sprintf(totalPackageString, "%d", packetFile.total_frag);
+    strcat(messageString, totalPackageString);
+    strncat(messageString, &separateColon, 1);
+    sprintf(fragNoString, "%d", packetFile.frag_no);
+    strcat(messageString, fragNoString);
+    strncat(messageString, &separateColon, 1);
+    sprintf(sizeString, "%d", packetFile.size);
+    strcat(messageString, sizeString);
+    strncat(messageString, &separateColon, 1);
+
+    strcat(messageString, packetFile.filename);
+    strncat(messageString, &separateColon, 1);
+
+    int partialSize = strlen(messageString);
+
+    memmove(messageString+strlen(messageString), packetFile.filedata, packetFile.size);
+    if(packetFile.frag_no == 816){
+        printf("converting result, %s\n", messageString);
+    }
+     //printf("converting result, %s\n", messageString);
+    return (partialSize + packetFile.size);
+}
+
+
--- server.c
+++ server.c
@@ -12,8 +12,6 @@
 
 #define MAXBUFLEN 1500
 
-
-
 struct packet{
     unsigned int total_frag; //total number of fragments
     unsigned int frag_no; //sequence number of the fragment
@@ -22,35 +20,15 @@ struct packet{
     char filedata[1000]; //actual data within the file
 };
 
-void stringTostruct(struct packet* packetFile, char* messageString){
-     char *token = strtok(messageString, ":");
-     int counter = 0;
-    while (token != NULL)
-    {
-        if(counter == 0){
-            packetFile->total_frag = atoi(token);
-        }else if(counter == 1){
-            packetFile->frag_no = atoi(token);
-        }else if(counter == 2){
-            packetFile->size = atoi(token);
-        }else if(counter == 3){
-            packetFile->filename = token;
-        }else{
-            memmove(packetFile->filedata, token, packetFile->size);
-        }
-        token = strtok(NULL, ":");
-        counter++;
-    }
-    return;
-}
+//transfer the message string into the packet for further analysis
+void stringTostruct(struct packet* packetFile, char* messageString, int messageSize);
 
 int main(int argc, char** argv){
     int socketfd;
     struct addrinfo hints, *res;
-    char buf[MAXBUFLEN];
+    //char buf[MAXBUFLEN];
     struct sockaddr_storage their_addr;
     socklen_t addr_len;
-
 
     // first, load up address structs with getaddrinfo():
     memset(&hints, 0, sizeof hints);//zero out the structure
@@ -81,27 +59,29 @@ int main(int argc, char** argv){
     int numbytes;
     bool continueReceive = true;
     FILE* fp;
+    bool firstTransmit = true;
     while(continueReceive){
+        char buf[MAXBUFLEN];
         if((numbytes = recvfrom(socketfd, buf, MAXBUFLEN - 1, 0,(struct sockaddr *)&their_addr, &addr_len)) == -1){//receive a message from client
             printf("Unable to receive from deliver. Quit the program\n");
             return 0;
         }
         buf[numbytes] = '\0';
         struct packet receivedpacketFile;
-        stringTostruct(&receivedpacketFile, buf);
-        printf("Recieve the message from deliver, seq#: %d\n", receivedpacketFile.frag_no);
-        char filePrefix[100] = "serverCreated";
-        char* fileNameBefore = receivedpacketFile.filename;
-        //printf("check file name, %s\n", fileNameBefore);
-        strcat(filePrefix, fileNameBefore);
-        //printf("check, %s\n", filePrefix);
-        fp = fopen(filePrefix, "w");
-        //printf("check data: \n");
-        for(int i = 0; i < receivedpacketFile.size; i++){
-            fputc(receivedpacketFile.filedata[i], fp);
-            //printf("%c", receivedpacketFile.filedata[i]);
+        printf("numbytes size, %d\n", numbytes);
+        stringTostruct(&receivedpacketFile, buf, numbytes);
+        printf("Recieve the message from deliver, seq#: %d, and package size is: %d\n", receivedpacketFile.frag_no, receivedpacketFile.size);
+        if(firstTransmit){
+            char filePrefix[100] = "serverCreated";
+            char* fileNameBefore = receivedpacketFile.filename;
+            printf("check file name, %s\n", fileNameBefore);
+            strcat(filePrefix, fileNameBefore);
+            printf("check, %s\n", filePrefix);
+            fp = fopen(filePrefix, "wb");
+            firstTransmit = false;
         }
-        //printf("\n");
+        //printf("package content, %s\n", receivedpacketFile.filedata);
+        fwrite(receivedpacketFile.filedata , 1 , receivedpacketFile.size , fp);
         if(sendto(socketfd, "ACK", 3, 0,(struct sockaddr *)&their_addr, addr_len) == -1){
             printf("Unable to send ACK number back to deliver. Quit the program\n");
             return 0;
@@ -110,11 +90,84 @@ int main(int argc, char** argv){
         if(receivedpacketFile.total_frag == receivedpacketFile.frag_no){//EOF the file reached
             continueReceive = false;
         }
-
     }
     fclose(fp);
+
     freeaddrinfo(res); //avoid memory leak
     close(socketfd);//close the socket
     return 0;
+}
 
+void stringTostruct(struct packet* packetFile, char* messageString, int messageSize){
+    int totalFragLength;
+    int fragNoLength;
+    int sizeLength;
+    int fileNameLength;
+    int counter = 0;
+    int currentLength = 0;
+    for(int i = 0; i < messageSize; i++){
+        if(messageString[i] == ':'){
+            if(counter == 0){
+                totalFragLength = currentLength;
+                counter++;
+            }else if(counter == 1){
+                fragNoLength = currentLength;
+                counter++;
+            }else if(counter == 2){
+                sizeLength = currentLength;
+                counter++;
+            }else if(counter == 3){
+                fileNameLength = currentLength;
+                counter++;
+                break;
+            }else{
+                break;
+            }
+            currentLength = 0;
+        }else{
+            currentLength++;
+        }
+
+    }
+    char totalFrag[totalFragLength + 1];
+    char fragNo[fragNoLength + 1];
+    char size[sizeLength + 1];
+    char fileName[fileNameLength + 1];
+    int pointer;
+    for(pointer = 0; pointer < totalFragLength; pointer++){
+        totalFrag[pointer] = messageString[pointer];
+    }
+    totalFrag[totalFragLength] = '\0';
+    packetFile->total_frag = atoi(totalFrag);
+    printf("total frag: %d\n", packetFile->total_frag);
+    for(pointer = 0; pointer < fragNoLength; pointer++){
+        fragNo[pointer] = messageString[pointer + totalFragLength + 1];
+    }
+    fragNo[fragNoLength] = '\0';
+    packetFile->frag_no = atoi(fragNo);
+    printf("frag no: %d\n", packetFile->frag_no);
+    for(pointer = 0; pointer < sizeLength; pointer++){
+        size[pointer] = messageString[pointer + totalFragLength + fragNoLength + 2];
+    }
+    size[sizeLength] = '\0';
+    packetFile->size = atoi(size);
+    printf("size: %d\n", packetFile->size);
+
+    for(pointer = 0; pointer < fileNameLength; pointer++){
+        fileName[pointer] = messageString[pointer + totalFragLength + fragNoLength + sizeLength + 3];
+    }
+    fileName[fileNameLength] = '\0';
+    packetFile->filename = (char *)malloc(sizeof(char) * fileNameLength);
+    packetFile->filename[fileNameLength] = '\0';
+    for(int i = 0; i < fileNameLength; i++){
+        packetFile->filename[i] = fileName[i];
+    }
+    printf("file name: %s\n", packetFile->filename);
+
+    int dataStart = totalFragLength + fragNoLength + sizeLength + fileNameLength + 4;
+    printf("datasize: %d\n", messageSize-dataStart+1);
+    for(int i = 0 ; i < packetFile->size; i++){
+        packetFile->filedata[i] = messageString[i + dataStart];
+    }
+    return;
 }
